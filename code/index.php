0e06adc13c98b2fc62de0bc4f32a2e9aac89ae39
Анастасия Шафирова <anastasiasafirova@MacBook-Pro-Anastasia.local>
2025-02-24 07:32:59 +0400

    1 домашнее, доделала

--- code/index.php
+++ code/index.php
@@ -10,3 +10,12 @@ var_dump($a == $b); ///data/mysite.расположение/index.php:9:boolean 
 var_dump((int)'012345'); ///data/mysite.расположение/index.php:10:int 12345 -- преобразует в число
 var_dump((float)123.0 == (int)123.0); ///data/mysite.расположение/index.php:11:boolean true -- данное выражение верно
 var_dump(0 == 'hello, world'); ///data/mysite.расположение/index.php:12:boolean false -- данное выражение не верно
+
+//в версии 7.4 последняя строчка будет равна true, так как строку он приравняет к 0, соответственно выражения будут равны
+
+$a = 1;
+$b = 2;
+
+$a = $a + $b; // a = 3
+$b = $a - $b; // b = 1
+$a = $a - $b; // a = 2, теперь a и b поменялись местами
